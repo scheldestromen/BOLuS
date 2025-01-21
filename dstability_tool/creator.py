157e1b93106ddc4f13597fefb16fd401cb2c29e8
KentropDevelopment <daniel.kentrop@kentropdev.com>
2025-01-21 10:29:39 +0100

    chore: refactor creator.py

--- dstability_tool/creator.py
+++ dstability_tool/creator.py
@@ -3,18 +3,121 @@ Creates d_stability_toolbox Model objects from the user input
 """
 from typing import List
 
-from dstability_toolbox.geometry import SurfaceLineCollection, CharPointsProfileCollection, create_geometries, \
-    CharPointType
+from dstability_toolbox.geometry import create_geometries, CharPointType, Geometry
 from dstability_toolbox.model import Model, Scenario, Stage
-from dstability_toolbox.soils import SoilCollection
-from dstability_toolbox.subsoil import Subsoil, subsoil_from_soil_profiles, SoilProfileCollection
+from dstability_toolbox.subsoil import subsoil_from_soil_profiles
 from dstability_toolbox.state import create_state_points_from_subsoil
 
 from input_reader import UserInputStructure
 
 
+def create_stage(
+    stage: dict,
+    scenario_name: str,
+    calc_name: str,
+    geometries: List[Geometry],
+    input_structure: UserInputStructure
+) -> Stage:
+    """
+    Creates a Stage object from the provided input.
+
+    Args:
+        stage: A dictionary containing stage details.
+        scenario_name: The name of the scenario.
+        calc_name: The name of the calculation.
+        geometries: A list of Geometry objects.
+        input_structure: The user-provided input structure.
+
+    Returns:
+        A Stage object.
+    """
+    geometry = next((geom for geom in geometries if geom.name == stage["geometry"]), None)
+
+    if geometry is None:
+        raise ValueError(f"Could not find geometry with name {stage['geometry']}")
+
+    surface_line = geometry.surface_line
+    profile_positions = input_structure.soil_profile_positions[stage["profile_position_name"]]
+
+    soil_profiles_and_coords = [
+        (sp, coord) for name, coord in profile_positions.items()
+        for sp in input_structure.soil_profiles.profiles
+        if sp.name == name
+    ]
+
+    # Create subsoil from the surfaceline, soil_profiles and the transitions
+    subsoil = subsoil_from_soil_profiles(
+        surface_line=surface_line,
+        soil_profiles=[sp[0] for sp in soil_profiles_and_coords],
+        transitions=[sp[1] for sp in soil_profiles_and_coords][1:],  # Skip the first coords, it's None
+    )
+
+    load = input_structure.loads.get_by_name(stage["load_name"]) if stage["load_name"] is not None else None
+
+    # Create the state points
+    state_points = create_state_points_from_subsoil(
+        subsoil=subsoil,
+        soil_collection=input_structure.soils,
+        state_type='POP'
+    ) if stage["apply_state_points"] else None
+
+    # Create the stage
+    return Stage(
+        name=stage["stage_name"],
+        notes="",
+        geometry=geometry,
+        subsoil=subsoil,
+        state_points=state_points,
+        load=load,
+        waternet=input_structure.waternets.get_waternet(
+            calc_name,
+            scenario_name,
+            stage['stage_name'])
+    )
+
+
+def create_scenario(
+    scenario: dict,
+    calc_name: str,
+    geometries: List[Geometry],
+    input_structure: UserInputStructure
+) -> Scenario:
+    """
+    Creates a Scenario object from the provided input.
+
+    Args:
+        scenario: A dictionary containing scenario details.
+        calc_name: The name of the calculation.
+        geometries: A list of Geometry objects.
+        input_structure: The user-provided input structure.
+
+    Returns:
+        A Scenario object.
+    """
+    stages = [create_stage(
+        stage=stage,
+        scenario_name=scenario["scenario_name"],
+        calc_name=calc_name,
+        geometries=geometries,
+        input_structure=input_structure
+    ) for stage in scenario["stages"]]
+
+    return Scenario(
+        name=scenario["scenario_name"],
+        notes="",
+        stages=stages
+    )
+
+
 def input_to_models(input_structure: UserInputStructure) -> List[Model]:
-    """Creates Model objects from the user input"""
+    """Converts user input into a list of Model objects.
+
+    Args:
+        input_structure: The user-provided input structure
+
+    Returns:
+        List[Model]: A list of Model objects.
+    """
     models: List[Model] = []
 
     # Create Geometry objects from the input
@@ -26,69 +129,10 @@ def input_to_models(input_structure: UserInputStructure) -> List[Model]:
 
     # Create a Model for each calculation dictionary
     for calc_config in input_structure.calc_configs:
-        scenarios = []
-
-        for scenario in calc_config["scenarios"]:
-            stages = []
-
-            for stage in scenario["stages"]:
-                geometry = next((geom for geom in geometries if geom.name == stage["geometry"]), None)
-
-                if geometry is None:
-                    raise ValueError(f"Could not find geometry with name {stage['geometry']}")
-
-                surface_line = geometry.surface_line
-                profile_positions = input_structure.soil_profile_positions[stage["profile_position_name"]]
-
-                soil_profiles_and_coords = [
-                    (sp, coord) for name, coord in profile_positions.items()
-                    for sp in input_structure.soil_profiles.profiles
-                    if sp.name == name
-                ]
-
-                # Create the subsoil from the surfaceline, soil_profiles and the positions
-                # TODO: Dit kan efficienter want sommige stages hebben dezelfde subsoil. Geen prio
-                subsoil = subsoil_from_soil_profiles(
-                    surface_line=surface_line,
-                    soil_profiles=[sp[0] for sp in soil_profiles_and_coords],
-                    transitions=[sp[1] for sp in soil_profiles_and_coords][1:],  # Skip the first coords, it's None
-                )
-                if stage["load_name"] is not None:
-                    load = input_structure.loads.get_by_name(stage["load_name"])
-
-                else:
-                    load = None
-
-                # Create the state points
-                if stage["apply_state_points"]:
-                    state_points = create_state_points_from_subsoil(
-                        subsoil=subsoil,
-                        soil_collection=input_structure.soils,
-                        state_type='POP'
-                    )
-                else:
-                    state_points = None
-
-                # Create the stage
-                stages.append(
-                    Stage(
-                        name=stage["stage_name"],
-                        notes="",
-                        geometry=geometry,
-                        subsoil=subsoil,
-                        state_points=state_points,
-                        load=load,
-                        waternet=input_structure.waternets.get_waternet(
-                            calc_config["calc_name"],
-                            scenario['scenario_name'],
-                            stage['stage_name'])
-                    ))
-
-            scenarios.append(Scenario(
-                name=scenario["scenario_name"],
-                notes="",
-                stages=stages
-            ))
+        scenarios = [
+            create_scenario(scenario, calc_config["calc_name"], geometries, input_structure)
+            for scenario in calc_config["scenarios"]
+        ]
 
         models.append(
             Model(
--- dstability_toolbox/loads.py
+++ dstability_toolbox/loads.py
@@ -7,7 +7,16 @@ from dstability_toolbox.geometry import CharPointType
 
 
 class Load(BaseModel):
-    """Representation of a uniform load"""
+    """Representation of a uniform load.
+
+    Attributes:
+        name: The name of the load.
+        magnitude: The magnitude of the load.
+        angle: The angle of the load distribution.
+        width: The width of the load.
+        position: The position of the load, characterised by a CharPointType.
+        direction: The direction of the load, either "inward" or "outward".
+    """
     name: str
     magnitude: float
     angle: float
@@ -17,6 +26,11 @@ class Load(BaseModel):
 
 
 class LoadCollection(BaseModel):
+    """Represents a collection of load instances.
+
+    Attributes:
+        loads: A list containing instances of the Load class.
+    """
     loads: list[Load]
 
     def get_by_name(self, name) -> Load:
